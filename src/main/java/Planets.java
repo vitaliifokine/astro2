ec871b6f014c41597c21422f4b36956237ec023c
Vitalii Fokin <vitaliiifokine@gmail.com>
2019-06-08 10:26:07 -0400

    Updates II

--- src/main/java/Planets.java
+++ src/main/java/Planets.java
@@ -6,6 +6,7 @@ public class Planets {
 
     static List<String> zodiac = Arrays.asList("ARI", "TAU", "GEM", "CAN", "LEO", "VIR", "LIB", "SCO", "SGR", "CAP", "AQR", "PSC");
     static List<String> characters = Arrays.asList("Choleric", "Sanguine", "Melancholic", "Phlegmatic");
+    static List<String> polarityEI = Arrays.asList("Extravert", "Introvert");
 
     public static HashMap<String, Integer> getPers(){
         HashMap<String, Integer> persantages = new HashMap<String, Integer>();
@@ -24,21 +25,40 @@ public class Planets {
     }
 
     public static HashMap<String, String> getTemperaments(){
-        HashMap<String, String> tempepaments = new HashMap<String, String>();
+        HashMap<String, String> temp = new HashMap<String, String>();
         /*Adding elements to HashMap*/
-        tempepaments.put(zodiac.get(0), characters.get(0));
-        tempepaments.put(zodiac.get(1), characters.get(2));
-        tempepaments.put(zodiac.get(2), characters.get(1));
-        tempepaments.put(zodiac.get(3), characters.get(3));
-        tempepaments.put(zodiac.get(4), characters.get(0));
-        tempepaments.put(zodiac.get(5), characters.get(2));
-        tempepaments.put(zodiac.get(6), characters.get(1));
-        tempepaments.put(zodiac.get(7), characters.get(3));
-        tempepaments.put(zodiac.get(8), characters.get(0));
-        tempepaments.put(zodiac.get(9), characters.get(2));
-        tempepaments.put(zodiac.get(10), characters.get(1));
-        tempepaments.put(zodiac.get(11), characters.get(3));
-        return tempepaments;
+        temp.put(zodiac.get(0), characters.get(0));
+        temp.put(zodiac.get(1), characters.get(2));
+        temp.put(zodiac.get(2), characters.get(1));
+        temp.put(zodiac.get(3), characters.get(3));
+        temp.put(zodiac.get(4), characters.get(0));
+        temp.put(zodiac.get(5), characters.get(2));
+        temp.put(zodiac.get(6), characters.get(1));
+        temp.put(zodiac.get(7), characters.get(3));
+        temp.put(zodiac.get(8), characters.get(0));
+        temp.put(zodiac.get(9), characters.get(2));
+        temp.put(zodiac.get(10), characters.get(1));
+        temp.put(zodiac.get(11), characters.get(3));
+        return temp;
+    }
+
+
+    public static HashMap<String, String> getExtraIntroversy(){
+        HashMap<String, String> polarity = new HashMap<String, String>();
+        /*Adding elements to HashMap*/
+        polarity.put(zodiac.get(0), polarityEI.get(0));
+        polarity.put(zodiac.get(1), polarityEI.get(1));
+        polarity.put(zodiac.get(2), polarityEI.get(0));
+        polarity.put(zodiac.get(3), polarityEI.get(1));
+        polarity.put(zodiac.get(4), polarityEI.get(0));
+        polarity.put(zodiac.get(5), polarityEI.get(1));
+        polarity.put(zodiac.get(6), polarityEI.get(0));
+        polarity.put(zodiac.get(7), polarityEI.get(1));
+        polarity.put(zodiac.get(8), polarityEI.get(0));
+        polarity.put(zodiac.get(9), polarityEI.get(1));
+        polarity.put(zodiac.get(10), polarityEI.get(0));
+        polarity.put(zodiac.get(11), polarityEI.get(1));
+        return polarity;
     }
 }
 
--- src/main/java/TemperamentPieChart.java
+++ src/main/java/TemperamentPieChart.java
@@ -4,18 +4,17 @@ import java.util.HashMap;
 public class TemperamentPieChart {
 
     public static void main(String args[]) {
-
         //test data start
-        String AscendantSign = "SGR";
+        String AscendantSign = "SCO";
         String SunSign = "VIR";
-        String MoonSign = "LEO";
-        String MercurySign = "LIB";
-        String VenusSign = "LIB";
-        String MarsSign = "SCO";
+        String MoonSign = "VIR";
+        String MercurySign = "LEO";
+        String VenusSign = "VIR";
+        String MarsSign = "GEM";
         String JupiterSign = "VIR";
-        String SaturnSign = "SCO";
-        String UranusSign = "SCO";
-        String NeptuneSign = "SCO";
+        String SaturnSign = "AQR";
+        String UranusSign = "CAP";
+        String NeptuneSign = "CAP";
         String PlutoSign = "SCO";
 
         HashMap<String, String> hmap = new HashMap<String, String>();
@@ -35,6 +34,9 @@ public class TemperamentPieChart {
 
         HashMap<String, Integer> result = calculateCharactes(hmap);
         result.forEach((key, value) -> System.out.println(key + " " + value));
+        System.out.println("------------");
+        HashMap<String, Integer> result2 = calculateExtraIntroVersy(hmap);
+        result2.forEach((key, value) -> System.out.println(key + " " + value));
     }
 
     public static HashMap<String, Integer> calculateCharactes(HashMap hashMap) {
@@ -47,6 +49,19 @@ public class TemperamentPieChart {
         hashMap.forEach((key, value) -> {
             int t = result.get(Planets.getTemperaments().get(value));
             result.put(Planets.getTemperaments().get(value),
+                    t + Planets.getPers().get(key));
+        });
+        return result;
+    }
+
+    public static HashMap<String, Integer> calculateExtraIntroVersy(HashMap hashMap) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+        result.put(Planets.polarityEI.get(0), 0);
+        result.put(Planets.polarityEI.get(1), 0);
+
+        hashMap.forEach((key, value) -> {
+            int t = result.get(Planets.getExtraIntroversy().get(value));
+            result.put(Planets.getExtraIntroversy().get(value),
                     t + Planets.getPers().get(key));
         });
         return result;
